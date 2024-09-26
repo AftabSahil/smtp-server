c4ebe99a0bb39ba4056d887b15d4f689d7da28ad
Sahil Aftab <150364167+AftabSahil@users.noreply.github.com>
2024-09-26 19:42:17 +0530

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,5 @@ passport.use(new GoogleStrategy({
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "/auth/google/callback"
 }, function(token, tokenSecret, profile, done) {
-  // Save the user to the database or find existing
-  // Use Prisma to manage user
   return done(null, profile);
 }));
